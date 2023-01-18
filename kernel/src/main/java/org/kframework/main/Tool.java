93b4f434f746ae437becf407d34f93a411e4b84a
rv-jenkins <devops@runtimeverification.com>
2023-01-18 15:10:50 +0000

    gh-pages: Updated the website

--- kernel/src/main/java/org/kframework/main/Tool.java
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2014-2019 K Team. All Rights Reserved.
-package org.kframework.main;
-
-public enum Tool {
-    KOMPILE, KAST, KRUN, KEQ, KPROVE, KBMC, KSEARCHPATTERN, KSERVER, KDEP
-}
